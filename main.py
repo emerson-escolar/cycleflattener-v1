3166c4dd6409ffe681aee8851ff49ba6841e861c
Emerson Escolar <38313244+emerson-escolar@users.noreply.github.com>
2026-01-30 17:10:34 +0900

    add initial files

---
+++ main.py
@@ -0,0 +1,9 @@
+import cycleflattener
+
+def main():
+    print("Hello from cycleflattener!")
+    print(cycleflattener.filtration.Filtration().x)
+
+
+if __name__ == "__main__":
+    main()
---
+++ src/cycleflattener/filtration.py
@@ -0,0 +1,11 @@
+
+
+class Filtration:
+    cycles: list
+    boundary_dict: dict[int, set]
+
+    def __init__(self):
+        self.vs
+        self.simplices
+        self.cycles
+        self.boundary_dict
