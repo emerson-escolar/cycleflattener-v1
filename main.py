f7ade7902b92a598eca0635a0881bad7d73a6ca7
Emerson Escolar <38313244+emerson-escolar@users.noreply.github.com>
2026-02-03 11:40:02 +0900

    better boundary matrix

--- main.py
+++ main.py
@@ -5,13 +5,37 @@ def main():
     foo = cycleflattener.filtration.Filtration()
 
     foo._load_vertices("./testdata/gen_sample_i2p.txt")
-    print(foo.vs)
+    print(foo.vertex_coordinates)
 
-    foo._load_alpha_simplices("./testdata/gen_sample_alphamap.txt")
+    foo._load_alpha_simplices("./testdata/gen_sample_alphamap.txt", maxbirth=9)
     print(foo.simplices)
 
-    foo._load_boundary_dict("./testdata/gen_sample_boundary.txt")
-    print(foo.boundary_dict)
+    foo._load_boundaries("./testdata/gen_sample_boundary.txt")
+    print(foo.boundaries)
+
+    print(foo.num_vertices)
+
+    print(foo.get_neighborhood_simplexindices([1], eps=4))
+
+    print("dim 0 boundary")
+
+    print(foo.get_simplexindices_satisfying(dim=0))
+    print(foo.get_boundary_matrix(0).toarray())
+    print(foo.get_simplexindices_satisfying(dim=-11))
+
+
+    print("\ndim 1 boundary")
+
+    print(foo.get_simplexindices_satisfying(dim=1))
+    print(foo.get_boundary_matrix(1).toarray())
+    print(foo.get_simplexindices_satisfying(dim=0))
+
+    print("\ndim 2 boundary")
+
+    print(foo.get_simplexindices_satisfying(dim=2))
+    print(foo.get_boundary_matrix(2).toarray())
+    print(foo.get_simplexindices_satisfying(dim=1))
+
 
 
 if __name__ == "__main__":
--- src/cycleflattener/filtration.py
+++ src/cycleflattener/filtration.py
@@ -1,55 +1,156 @@
 import numpy as np
 import ast
+import scipy.spatial.distance as ssd
+import scipy.sparse as ssm
+
+
+def list_to_lookupdict(x:list):
+    return {v:k for k,v in enumerate(x)}
+
 
 class Filtration:
-    vs: np.array
-    simplices: dict[int, dict]
+    vertex_coordinates: np.array
+    simplices: list[dict[str, any]]
+    boundaries: list[dict[int, float]]
     cycles: list
-    boundary_dict: dict[int, set]
+
+    KEY_birth="b"
+    KEY_dim="d"
+    KEY_vlist="v"
 
     def __init__(self):
-        self.vs = np.array([])
-
-        self.simplices = {}
-        self.cycles = []
+        self.vertex_coordinates = np.array([])
+        self.simplices = []
         self.boundary_dict = {}
 
+        self.cycles = []
+
+
+    @property
+    def num_vertices(self):
+        return self.vertex_coordinates.shape[0]
+
+    @property
+    def num_simplices(self):
+        return len(self.simplices)
+
+    @property
+    def last_birth(self):
+        return max(self.get_simplex_birth(simplexindex)
+                   for simplexindex in range(self.num_simplices))
 
     def _load_vertices(self, fname: str):
         # expects "i2p" file
         # format: vertexindex x y z r
-        self.vs = np.loadtxt(fname, usecols=[1, 2, 3])
+        self.vertex_coordinates = np.loadtxt(fname, usecols=[1, 2, 3])
 
 
-    def _load_alpha_simplices(self, fname:str):
+    def _load_alpha_simplices(self, fname:str, maxbirth=np.inf):
         # expects "alphamap" file
         # format: dim birth (comma-separated vertex indices)
 
+        def __alphamap_processor(ifile):
+            for line in ifile:
+                part = line.split(" ")
+                if float(part[1]) <= maxbirth:
+                    yield {type(self).KEY_dim: int(part[0]),
+                           type(self).KEY_birth: float(part[1]),
+                           type(self).KEY_vlist: [int(v) for v in part[2].split(",")]}
+                else:
+                    break
+
         with open(fname) as ifile:
-            lines = ifile.read().splitlines()
-
-        self.simplices = {}
-        for index, line in enumerate(lines):
-            part = line.split(" ")
-            dim = int(part[0])
-            birth = float(part[1])
-            vertex_list = [int(v) for v in part[2].split(",")]
-            simplex = {"dim": dim, "birth": birth, "vertex_list": vertex_list}
-            self.simplices.update({index: simplex})
+            self.simplices = list(__alphamap_processor(ifile))
 
 
-    def _load_boundary_dict(self, fname:str):
+    def _load_boundaries(self, fname:str):
         # expects "boundary" file
         # format: simplexindex : {(simplexindex, coeff), ... }
 
-        with open(fname) as f:
-            lines = f.read().splitlines()
+        def __boundary_processor(ifile):
+            for i, line in enumerate(ifile):
+                part = line.split(":")
+                simplex_index = int(part[0])
+                assert(simplex_index == i)
+                if simplex_index >= self.num_simplices:
+                    break
+                yield {entry[0]:entry[1] for entry in ast.literal_eval(str(part[1]))}
 
-        # TODO: think about data structure for boundary_dict...
-        self.boundary_dict = {}
-        for line in lines:
-            part = line.split(":")
-            simplex_index = int(part[0])
+        with open(fname) as ifile:
+            self.boundaries = list(__boundary_processor(ifile))
 
-            bd_sets = ast.literal_eval(str(part[1]))
-            self.boundary_dict.update({simplex_index: bd_sets})
+
+    def get_simplexindices_satisfying(self, dim:int, maxbirth=np.inf) -> list:
+        return [i for i, simplex in enumerate(self.simplices) if
+                (simplex[type(self).KEY_birth] <= maxbirth) and
+                (simplex[type(self).KEY_dim] == dim)]
+
+    def get_simplex_birth(self, simplexindex:int) -> float:
+        return self.simplices[simplexindex][type(self).KEY_birth]
+    def get_simplex_dim(self, simplexindex:int) -> int:
+        return self.simplices[simplexindex][type(self).KEY_dim]
+    def get_simplex_vlist(self, simplexindex:int) -> list[int]:
+        return self.simplices[simplexindex][type(self).KEY_vlist]
+
+    def get_exterior_angle(self, simplexindex1:int, simplexindex2:int) -> float:
+        if self.get_simplex_dim(simplexindex1) != 1 or self.get_simplex_dim(simplexindex2) != 1:
+            print(f"ERROR: get_exterior_angle called on non-edge(s): {simplexindex1} and {simplexindex2}")
+            return -1.0
+
+        # TODO
+        return 0.0
+
+
+
+    def get_neighborhood_simplexindices(self, vertexindices:list, eps:float=0.0,
+                                        candidate_simplexindices:list|None=None) -> list:
+        # From the list of candidate_simplexindices,
+        # extract the sublist of simplexindices whose simplex has each vertex
+        # at most distance eps from the set of vertexindices.
+
+        dist_vs = ssd.cdist(self.vertex_coordinates[vertexindices], self.vertex_coordinates)
+        neighborhood_vertexindices = {vertexindex for vertexindex in range(self.num_vertices) if
+                                      np.min(dist_vs[:, vertexindex]) <= eps}
+
+        if candidate_simplexindices is None: candidate_simplexindices = range(self.num_simplices)
+        def __generator(neighborhood_vertexindices, candidate_simplexindices):
+            for simplexindex in candidate_simplexindices:
+                if set(self.get_simplex_vlist(simplexindex)).issubset(neighborhood_vertexindices):
+                    yield simplexindex
+
+        return list(__generator(neighborhood_vertexindices, candidate_simplexindices))
+
+
+    def get_boundary_coeff(self, query_simplexindex, face_simplexindex):
+        if face_simplexindex in self.boundaries[query_simplexindex]:
+            return self.boundaries[query_simplexindex][face_simplexindex]
+        else:
+            return 0
+
+    def get_boundary_matrix(self, dim:int):
+        cur_simplices = self.get_simplexindices_satisfying(dim=dim)
+        dwn_simplices = self.get_simplexindices_satisfying(dim=dim-1)
+
+        cur_rev = list_to_lookupdict(cur_simplices)
+        dwn_rev = list_to_lookupdict(dwn_simplices)
+
+        csr_data = []
+        csr_row_indices = []
+        csr_col_indices = []
+
+        for simplexindex in cur_simplices:
+            for face_simplexindex in self.boundaries[simplexindex]:
+                csr_data.append(self.boundaries[simplexindex][face_simplexindex])
+                csr_col_indices.append( cur_rev[simplexindex] )
+                csr_row_indices.append( dwn_rev[face_simplexindex] )
+
+        return ssm.csr_matrix((csr_data, (csr_row_indices,csr_col_indices)),
+                              shape=(len(dwn_simplices),len(cur_simplices)))
+
+
+
+
+    def get_exterior_angles_matrix(self):
+        edges = self.get_simplexindices_satisfying(dim=1)
+
+        # TODO
