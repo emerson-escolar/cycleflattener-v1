3bddd4e5ff581bf1519a62be2607929454fb9c18
Emerson Escolar <38313244+emerson-escolar@users.noreply.github.com>
2026-05-21 21:55:12 +0900

    update scripts for experiments

---
+++ cplex_config_60s_mem_18deg.py
@@ -0,0 +1,17 @@
+import numpy as np
+
+# context.cplex_parameters.preprocessing.qtolin.set(1)
+context.cplex_parameters.timelimit = 60 # in seconds
+# context.cplex_parameters.randomseed(42)
+# context.cplex_parameters.parallel = 1
+context.cplex_parameters.workmem = 2048*8
+
+context.cplex_parameters.mip.strategy.file = 1 # Node file in memory and compressed; default
+#context.cplex_parameters.mip.strategy.file = 3 # Node file on disk and compressed
+
+context.cplex_parameters.mip.strategy.nodeselect = 2 # best-estimate search
+context.cplex_parameters.mip.tolerances.absmipgap = np.pi * 0.1 # 18 deg total departure
+context.cplex_parameters.mip.display = 1
+
+# if qcr_shift:
+#     context.cplex_parameters.optimalitytarget = 1  # globally optimal solution to a convex model.
--- replication/main_script.sh
+++ replication/main_script.sh
@@ -12,12 +12,37 @@ cd $projectDir
 BS=20
 FS=20
 FR=10
-DATA=slipper_2_1_1_$BS\_$FS\_$FR
-echo $DATA
+DATA=slipper_2_1_1_${BS}_${FS}_${FR}
 
-uv run ./tests/generate_testdata.py --bs $BS --fs $FS --fr $FR -o $replDir
-optiperslp -e -z $replDir/$DATA/$DATA.txt -p $replDir/$DATA
-uv run main.py -i $replDir/$DATA $DATA -c ./cplex_config_quick.py
 
+# data generation and optiperslp output should be deterministic.
+# (if noise added in future, hash this too)
+FOLDER=${DATA}
+echo $FOLDER
+
+if [ ! -f "$replDir/$FOLDER/$DATA.txt" ]; then
+    echo "$replDir/$FOLDER/$DATA.txt not found; generating data..."
+    uv run ./tests/generate_testdata.py --bs $BS --fs $FS --fr $FR -o $replDir/$FOLDER
+else
+    echo "$replDir/$FOLDER/$DATA.txt found."
+fi
+
+if [ ! -f "$replDir/$FOLDER/gen_${DATA}_alphamap.txt" ]; then
+    echo "$replDir/$FOLDER/gen_${DATA}_alphamap.txt not found; applying optiperslp..."
+    optiperslp -e -z $replDir/$FOLDER/$DATA.txt -p $replDir/$FOLDER
+else
+    echo "$replDir/$FOLDER/gen_$DATA_alphamap.txt found."
+fi
+
+# Hash output folders for different runs
+# UUID version 7 was introduced in python 3.14...
+# but cplex uses python 3.10
+UUID=$(uv run -p 3.14 --no-project -m uuid -u uuid7)
+
+for timeLimit in 5; do
+    optiOutDir=$replDir/$FOLDER/$(printf "%03d" $timeLimit)sec_$UUID
+    mkdir $optiOutDir
+    /usr/bin/time -v uv run main.py -i $replDir/$FOLDER -o $optiOutDir $DATA -c ./cplex_config_60s_mem_18deg.py -t $timeLimit 2>&1 | tee $optiOutDir/output.log
+done
 
 cd $origDir
