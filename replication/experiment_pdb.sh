0a5b6cdffcd61593a7f0d7aefa5e44444189abbb
Emerson Escolar <38313244+emerson-escolar@users.noreply.github.com>
2026-05-28 21:33:45 +0900

    add experiment_pdb

---
+++ replication/experiment_pdb.sh
@@ -0,0 +1,46 @@
+#!/bin/env bash
+
+CODE=${1}
+timeLimit=5
+nSolve=20
+
+origDir=$(pwd)
+echo $origDir
+
+replDir=$(dirname -- "$(readlink -f -- "$BASH_SOURCE")")
+projectDir=$replDir/../
+echo $projectDir
+
+cd $projectDir
+
+DATA=pdb_${CODE}
+
+# data generation and optiperslp output should be deterministic.
+UUID=$(uv run -p 3.14 --no-project -m uuid -u uuid7)
+FOLDER=${DATA}
+echo $FOLDER
+
+if [ ! -f "$replDir/$FOLDER/$DATA.txt" ]; then
+    echo "$replDir/$FOLDER/$DATA.txt not found; generating data..."
+    uv run ./tests/generate_testdata.py pdb $CODE -o $replDir/$FOLDER
+else
+    echo "$replDir/$FOLDER/$DATA.txt found."
+fi
+
+if [ ! -f "$replDir/$FOLDER/gen_${DATA}_alphamap.txt" ]; then
+    echo "$replDir/$FOLDER/gen_${DATA}_alphamap.txt not found; applying optiperslp..."
+    optiperslp -e -z $replDir/$FOLDER/$DATA.txt -p $replDir/$FOLDER
+else
+    echo "$replDir/$FOLDER/gen_$DATA_alphamap.txt found."
+fi
+
+# Hash output folders for different runs
+# UUID version 7 was introduced in python 3.14...
+# but cplex uses python 3.10
+UUID=$(uv run -p 3.14 --no-project -m uuid -u uuid7)
+
+optiOutDir=$replDir/$FOLDER/$(printf "%04d" $timeLimit)sec_$UUID
+mkdir $optiOutDir
+/usr/bin/time -v uv run main.py -i $replDir/$FOLDER -o $optiOutDir $DATA -c ./cplex_config_60s_mem_18deg.py -t $timeLimit -n $nSolve 2>&1 | tee $optiOutDir/output.log
+
+cd $origDir
