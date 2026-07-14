e658fa3db099e9a3b98f03271f00d7f110afb36b
Emerson Escolar <38313244+emerson-escolar@users.noreply.github.com>
2026-07-14 14:41:07 +0900

    add script for analyzing results of experiment_repeated

---
+++ replication/repeated_parser.py
@@ -0,0 +1,103 @@
+import pathlib
+import argparse
+import textwrap
+
+import glob
+import json
+
+import cycleflattener.utils.saveload as sl
+
+import numpy as np
+import pandas as pd
+
+import matplotlib.pyplot as plt
+
+
+def construct_parser() -> argparse.ArgumentParser:
+    desc = textwrap.dedent('''\
+    Program for parsing repeated angle optimization results.
+    ''')
+
+    parser = argparse.ArgumentParser(description=desc,
+                                     formatter_class=argparse.RawDescriptionHelpFormatter)
+
+    parser.add_argument("inputdir", type=pathlib.Path,
+                        help="path to directory where outputs are stored",
+                        default=pathlib.Path.cwd())
+
+    parser.add_argument("--outputdir", "-o", type=pathlib.Path,
+                        help="path to directory where outputs are stored",
+                        default=None)
+
+    parser.add_argument("--bins", "-b", type=int,
+                        help="number of bins for histograms",
+                        default=10)
+
+    parser.add_argument('--show', action="store_true",
+                        help='Show visualizations')
+
+    return parser
+
+
+
+def parse_output(fp):
+    data = sl.CyclesFileV2(**json.load(fp))
+    n = len(data.cycles[-1].cycle)
+    l = data.cycles[-1].length
+    k = data.cycles[-1].kappa
+
+    return n,l,k
+
+
+def plot_histo(data:pd.Series, bins:int, xlabel:str, name:str, outputdir:pathlib.Path, show=False):
+    f, ax = plt.subplots(figsize=(12.8, 9.6))
+    data.plot.hist(ax=ax, bins=bins)
+    plt.xlabel(xlabel)
+    spath = outputdir / f"{name}.pdf"
+    plt.savefig(spath)
+
+    if show:
+        plt.show(block=True)
+
+def main():
+    args = construct_parser().parse_args()
+
+    if args.outputdir is None:
+        outputdir = args.inputdir
+    else:
+        outputdir = args.outputdir
+
+    ks = []
+    ls = []
+    ns = []
+    for log in args.inputdir.glob('**/*v2.json'):
+        with open(log, "r") as fp:
+            n,l,k = parse_output(fp)
+            ns.append(n)
+            ls.append(l)
+            ks.append(k)
+
+
+    plot_histo((pd.Series(ks) / np.pi), args.bins,
+               "total absolute curvature (over pi)", "hist_kappa",
+               outputdir, args.show)
+
+    plot_histo(pd.Series(ls), args.bins,
+               "cycle lengths", "hist_lengths",
+               outputdir, args.show)
+
+    plot_histo(pd.Series(ns), args.bins,
+               "cycle sizes (number of 1-simplices)", "hist_sizes",
+               outputdir, args.show)
+
+    with open(outputdir/ "kappas.csv", "w") as fp:
+        for k in ks:
+            fp.write(f"{k / np.pi} \\pi\n")
+
+    print(f"Best kappa: {min(ks)/np.pi} \\pi")
+
+
+
+
+if __name__ == "__main__":
+    main()
