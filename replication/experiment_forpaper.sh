ccc8fa3e5da81da9ff3e87217e781344ad5a6044
Emerson Escolar <38313244+emerson-escolar@users.noreply.github.com>
2026-07-25 23:36:41 +0900

    parametrize experiment_forpaper

--- replication/experiment_forpaper.sh
+++ replication/experiment_forpaper.sh
@@ -1,16 +1,25 @@
 #!/usr/bin/env bash
 
-#S1
+S1() {
+    local NAME=slipper_2_1_1_20_20_10
+    local TIME=1
+    local NSOLVE=3
 
-NAME=slipper_2_1_1_20_20_10
-# ./experiment_slipper.sh --timeLimit 1 --nSolve 3 --nouuid 20 20 10
-# ./experiment_repeated.sh --timeLimit 1 --nSolve 3 --ratio 0.2 --nouuid ${NAME}/ ${NAME}
-# uv run ./repeated_parser.py ./${NAME}/0001sec_0.2ratio_repeats
+    # basic run
+    ./experiment_slipper.sh --timeLimit ${TIME} --nSolve ${NSOLVE} --nouuid 20 20 10
 
-COLL=./${NAME}/${NAME}_collected.txt
+    # collect results for basic runs
+    local COLL=./${NAME}/${NAME}_collected.txt
+    for tt in 0.1 0.2 0.4; do
+	echo "**************************************************" >> ${COLL}
+	echo ${tt} >> ${COLL}
+	uv run cf_tabler ./${NAME}/${NAME}.txt ./${NAME}/$(printf "%04d" ${TIME})sec_${tt}ratio/${NAME}_0th_solutions_v2.json >> ${COLL}
+    done
 
-for tt in 0.1 0.2 0.4; do
-    echo "**************************************************" >> ${COLL}
-    echo ${tt} >> ${COLL}
-    uv run cf_tabler ./${NAME}/${NAME}.txt ./${NAME}/0001sec_${tt}ratio/${NAME}_0th_solutions_v2.json >> ${COLL}
-done
+    # repeated runs. hardcoded to do 0.2
+    ./experiment_repeated.sh --timeLimit ${TIME} --nSolve ${NSOLVE} --ratio 0.2 --nouuid ${NAME}/ ${NAME}
+    uv run ./repeated_parser.py ./${NAME}/$(printf "%04d" ${TIME})sec_0.2ratio_repeats
+}
+
+
+S1
